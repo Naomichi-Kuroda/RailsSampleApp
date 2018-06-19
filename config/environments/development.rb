c9210b6c84df83c9ce50cfb5290b8c863935f719
Naomichi Kuroda <naomichi.kuroda.jp@gmail.com>
2018-06-20 01:30:42 +0900

    Add ActionMailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,15 @@ Rails.application.configure do
     port: '3000'
   }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: Rails.application.credentials[Rails.env.to_sym][:SMTP_EMAIL],
+    password: Rails.application.credentials[Rails.env.to_sym][:SMTP_PASSWORD]
+  }
+
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
